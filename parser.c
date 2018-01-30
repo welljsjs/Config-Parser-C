6bb8fbc21d332701cb51d5b7d23b9b436c46ece5
welljsjs <js@spraken.de>
2018-01-30 19:25:46 +0100

    Create parser.c

---
+++ parser.c
@@ -0,0 +1,38 @@
+typedef struct config_option config_option;
+typedef config_option* config_option_t;
+
+struct config_option {
+    config_option_t prev;
+    char key[CONFIG_ARG_MAX_BYTES];
+    char value[CONFIG_ARG_MAX_BYTES];
+};
+
+config_option_t read_config_file(char* path) {
+    FILE* fp;
+    
+    if ((fp = fopen(path, "r+")) == NULL) {
+        perror("fopen()");
+        return NULL;
+    }
+    
+    config_option_t last_co_addr = NULL;
+    
+    while(1) {
+        config_option_t co = NULL;
+        if ((co = calloc(1, sizeof(config_option))) == NULL)
+            continue;
+        memset(co, 0, sizeof(config_option));
+        co->prev = last_co_addr;
+        
+        if (fscanf(fp, "%s = %s", &co->key[0], &co->value[0]) != 2) {
+            if (feof(fp)) {
+                break;
+            }
+            perror("fscanf()");
+            continue;
+        }
+        //printf("Key: %s\nValue: %s\n", co->key, co->value);
+        last_co_addr = co;
+    }
+    return last_co_addr;
+}
